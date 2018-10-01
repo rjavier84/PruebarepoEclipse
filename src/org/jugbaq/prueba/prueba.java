50e7430188560aed56b12a9a06cf77a57096883a
Javier Rodriguez <rjavier84@gmail.com>
2018-10-01 11:54:17 -0500

    prueba de eclipse en Github

---
+++ src/org/jugbaq/prueba/prueba.java
@@ -0,0 +1,10 @@
+package org.jugbaq.prueba;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba de Github usando Eclipse");
+	}
+
+}
